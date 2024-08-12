3871a84e22da3aa6b0abd4cc9e45ba659e9be86a
MdAshrafUllah <96839511+MdAshrafUllah@users.noreply.github.com>
2024-08-12 19:24:36 +0600

    file Structure

--- lib/presentation/ui/utility/assets_path.dart
+++ lib/presentation/ui/utility/assets_path.dart
@@ -1,3 +1,4 @@
 class AssetsPath {
-  static String downArrow = 'assets/icons/Down Arrow.svg';
+  static String downArrow = "assets/icons/Down Arrow.svg";
+  static String envelope = "assets/icons/envelope.svg";
 }
