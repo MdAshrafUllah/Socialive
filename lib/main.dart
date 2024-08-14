cc9760d7c13fbc25049fb7c374efe368601f2086
Rayhan Sany <124179417+Rayhan-Sany@users.noreply.github.com>
2024-08-15 01:05:05 +0600

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:socialive/app/app.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Socialive());
 }
