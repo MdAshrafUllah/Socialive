2799f19aa7f612b2602238de0af53f1d92e7dcf8
Anup Barua <anupbarua30@gmail.com>
2024-08-15 11:40:39 +0600

    Create text_field_widget.dart
    create

---
+++ lib/presentation/ui/widgets/text_field_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class TextFieldWidget extends StatelessWidget {
+  const TextFieldWidget({
+    super.key, 
+    required this.controllerTE, 
+    required this.hint, 
+    this.obscure=false
+  });
+  final TextEditingController controllerTE;
+  final String hint;
+  final bool obscure;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controllerTE,
+      obscureText: obscure,
+      decoration: InputDecoration(
+        hintText: hint,
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.grey, width: 1),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.blue, width: 1),
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+}
